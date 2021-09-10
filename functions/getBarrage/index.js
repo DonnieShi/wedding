3db3422eda5492ba6ba8fa9113add799be31ba57
wc.shi <wc.shi@upltv.com>
2021-09-10 13:40:24 +0800

    update

--- functions/getBarrage/index.js
+++ functions/getBarrage/index.js
@@ -5,6 +5,20 @@ const app = tcb.init({ env: 'wedding-8golxnc0da734bc5' })
 const db = app.database()
 const barrageCollection = db.collection('wedding-invitation-barrage')
 
+
+const auth = app.auth();
+
+async function login(){
+  await auth.anonymousAuthProvider().signIn();
+  // 匿名登录成功检测登录状态isAnonymous字段为true
+  const loginState = await auth.getLoginState();
+  console.log(loginState.isAnonymousAuth); // true
+}
+
+login();
+
+
+
 exports.main = async () => {
   let result
 
